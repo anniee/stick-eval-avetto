f7904644c22edfcfa97585442bbcf99c8085d4b9
Anne Vetto <vettoap@gmail.com>
2018-12-12 02:07:43 -0800

    Add filter field to search for names by char

--- src/app/App.js
+++ src/app/App.js
@@ -17,10 +17,18 @@ class App extends Component {
     })
   }
   render() {
+
+    const names = [{"name":"Yasujiro"},{"name":"F.W."},{"name":"Sergei"},{"name":"Carl"},{"name":"Howard"},{"name":"Luis"},{"name":"Martin"},
+    {"name":"Billy"},{"name":"Charles"},{"name":"Kar-Wai"},{"name":"Jean-Luc"},{"name":"Ingmar"},{"name":"Francis"},{"name":"Akira"},
+    {"name":"Jean"},{"name":"John"},{"name":"Federico"},{"name":"Stanley"},{"name":"Alfred"},{"name":"Orson"},{"name":"Alexander"},
+    {"name":"Frank"},{"name":"Ingrid"},{"name":"Horatio"},{"name":"Akiva"},{"name":"Luciano"},{"name":"George"},{"name":"Stacey"},
+    {"name":"Horton"},{"name":"Carrie"},{"name":"Alfonse"},{"name":"Yasmin"}
+    ];
+
     return (
       <div style={{fontFamily: 'Roboto', textAlign: 'center'}}>
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet"></link>
-        <NamesList callback={this.handleNamesSelection.bind(this)} />
+        <NamesList names={names} callback={this.handleNamesSelection.bind(this)} />
         <NamesDisplay names={this.state.names} />
       </div>
     );
--- src/app/NamesList.js
+++ src/app/NamesList.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 
 class NamesList extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    };
+  }
+
+  updateSearch(e) {
+    this.setState({search: e.target.value.substr(0,20)});
+  }
+
   getContent(e) {
     e.preventDefault();
     let selectedNames = document.querySelectorAll('input[name="name-check"]:checked');
@@ -12,25 +23,24 @@ class NamesList extends Component {
     };
 
     let selectedNamesStr = selectedNamesArr.join(', ');
-    console.log(selectedNamesStr);
     this.props.callback(selectedNamesStr);
   }
 
   render() {
-    const data =[{"name":"Yasujiro"},{"name":"F.W."},{"name":"Sergei"},{"name":"Carl"},{"name":"Howard"},{"name":"Luis"},{"name":"Martin"},
-    {"name":"Billy"},{"name":"Charles"},{"name":"Kar-Wai"},{"name":"Jean-Luc"},{"name":"Ingmar"},{"name":"Francis"},{"name":"Akira"},
-    {"name":"Jean"},{"name":"John"},{"name":"Federico"},{"name":"Stanley"},{"name":"Alfred"},{"name":"Orson"},{"name":"Alexander"},
-    {"name":"Frank"},{"name":"Ingrid"},{"name":"Horatio"},{"name":"Akiva"},{"name":"Luciano"},{"name":"George"},{"name":"Stacey"},
-    {"name":"Horton"},{"name":"Carrie"},{"name":"Alfonse"},{"name":"Yasmin"}
-    ];
+    let filteredNames = this.props.names.filter(
+        (name) => {
+          return name.name.indexOf(this.state.search) !== -1;
+        }
+      );
 
     return (
       <div style={{float: 'left', margin: '0 auto', width: '50%'}}>
         <h2>Names</h2>
+        <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)} />
         <form>
           <ul style={{listStyle: 'none', padding: '0'}}>
-            {data.map(function(d, idx){
-              return (<li key={idx}><input name="name-check" type="checkbox" value={d.name} id={"dir-"+ (idx + 1)} /><label htmlFor={"dir-"+ (idx + 1)} style={{cursor: 'pointer', display: 'inline-block', margin: '0 auto', width: '35%'}}>{d.name}</label></li>)
+            {filteredNames.map(function(d, i){
+              return (<li key={i}><input name="name-check" type="checkbox" value={d.name} id={"dir-"+ (i + 1)} /><label htmlFor={"dir-"+ (i + 1)} style={{cursor: 'pointer', display: 'inline-block', margin: '0 auto', width: '35%'}}>{d.name}</label></li>)
             })}
           </ul>
           <button type="submit" onClick={this.getContent.bind(this)}>Display Selected Names</button>
